1e10d9fef80c9974c9f138f1917a98ebc0629021
Lrnc_Olg <laurence@example.com>
2018-11-25 11:58:32 +0100

    Remove imageColoring from SimpleOperation

--- src/tools/SimpleOperation.java
+++ src/tools/SimpleOperation.java
@@ -1,57 +1,8 @@
 package tools;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class SimpleOperation {
-    /** Modify color composition of a bufferedImage.
-     *
-     * @param myColor is the color to increase/decrease.
-     * @param myBufferedImage is the bufferedImage to color.
-     * @param ratio is the ratio to apply at the color concerned.
-     * @return a bufferedImage with rgb values modified.
-     */
-    public static BufferedImage imageColoring(String myColor, BufferedImage myBufferedImage, double ratio) {
-        int decalage;
-        int colorToChange;
-        int secondColor;
-        int thirdColor;
-        int maxX = myBufferedImage.getWidth();
-        int maxY = myBufferedImage.getHeight();
-
-        for (int x = 0; x < maxX; x++){
-            for(int y = 0; y < maxY; y++) {
-
-                Color myPixelColor = new Color(myBufferedImage.getRGB(x, y));
-
-                switch (myColor) {
-                    case "R":
-                        decalage = 16;
-                        colorToChange = myPixelColor.getRed();
-                        secondColor = myPixelColor.getGreen()<<8;
-                        thirdColor = myPixelColor.getBlue();
-                        break;
-                    case "G":
-                        decalage = 8;
-                        colorToChange = myPixelColor.getGreen();
-                        secondColor = myPixelColor.getRed()<<16;
-                        thirdColor = myPixelColor.getBlue();
-                        break;
-                    default:
-                        decalage = 0;
-                        colorToChange = myPixelColor.getBlue();
-                        secondColor = myPixelColor.getGreen()<<8;
-                        thirdColor = myPixelColor.getRed()<<16;
-                        break;
-                }
-                colorToChange =( Math.min((int)(colorToChange*ratio),255) )<< decalage;
-                myBufferedImage.setRGB( x , y ,( colorToChange + secondColor + thirdColor ) );
-            }
-
-        }
-        return myBufferedImage;
-    }
-
     /**
      * Create a copy of BufferedImage.
      *
@@ -92,5 +43,4 @@ public class SimpleOperation {
         }
         return myStr.toString();
     }
-
 }
